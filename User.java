d7f852f65d74441c437aaa80816256237cbf062c
maeng3 <thdnf0323@naver.com>
2025-05-10 18:11:01 +0900

    Update User.java
    add get method

--- User.java
+++ User.java
@@ -1,8 +1,8 @@
 class User{
-	int age;
-	float weight;
-	boolean gender;
-	float height;
+	private int age;
+	private float weight;
+	private boolean gender;
+	private float height;
 	
 	public User(int age, float weight, boolean gender, float height) {
 		this.age=age;
@@ -14,5 +14,21 @@ class User{
 	public double getBMI() {
 	    double bmi = 1.3 * (weight / Math.pow(height / 100.0, 2.5));
 	    return Math.round(bmi * 10) / 10.0;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public float getWeight() {
+		return weight;
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public boolean getGen() {
+		return gender;
 	}
 }
