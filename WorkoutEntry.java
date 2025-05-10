0cc1dc2df7017169d961766579f9805e0ec3c82d
maeng3 <thdnf0323@naver.com>
2025-05-10 17:56:59 +0900

    Update WorkoutEntry.java
    edit fromCSV, add LocalDate variable

--- WorkoutEntry.java
+++ WorkoutEntry.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
 class WorkoutEntry {
     private Exercise exercise;
     private int weight;
     private int reps;
     private int sets;
+    private LocalDate date;
 
-    public WorkoutEntry(Exercise exercise, int weight, int reps, int sets) {
+    public WorkoutEntry(Exercise exercise, int weight, int reps, int sets, LocalDate date) {
         this.exercise = exercise;
         this.weight = weight;
         this.reps = reps;
         this.sets = sets;
+        this.date=date;
     }
 
     public Exercise getExercise() {
@@ -30,6 +33,10 @@ class WorkoutEntry {
     public int getTotalWeight() {
         return weight * reps * sets;
     }
+    
+    public LocalDate getDate() {
+    	return date;
+    }
 
     public String toCSV() {
         return String.join(",",
@@ -43,32 +50,36 @@ class WorkoutEntry {
 
     public static WorkoutEntry fromCSV(String[] data) {
         Exercise exercise;
-        switch (data[0]) {
+        LocalDate date = LocalDate.parse(data[0]); // 날짜 파싱
+        switch (data[1]) {
             case "ChestExercise": 
-                exercise = new ChestExercise(data[1]);
+                exercise = new ChestExercise(data[2]);
                 break;
             case "BackExercise":
-                exercise = new BackExercise(data[1]);
+                exercise = new BackExercise(data[2]);
                 break;
             case "LegExercise":
-                exercise = new LegExercise(data[1]);
+                exercise = new LegExercise(data[2]);
                 break;
             case "ShoulderExercise":
-                exercise = new ShoulderExercise(data[1]);
+                exercise = new ShoulderExercise(data[2]);
                 break;
             case "ArmExercise":
-                exercise = new ArmExercise(data[1]);
+                exercise = new ArmExercise(data[2]);
                 break;
             case "AbsExercise":
-                exercise = new AbsExercise(data[1]);
+                exercise = new AbsExercise(data[2]);
                 break;
             default:
-                exercise = new ChestExercise(data[1]); // fallback
+                exercise = new ChestExercise(data[2]); // fallback
         }
-        return new WorkoutEntry(exercise,
-                Integer.parseInt(data[2]),
-                Integer.parseInt(data[3]),
-                Integer.parseInt(data[4])
+
+        return new WorkoutEntry(
+                exercise,
+                Integer.parseInt(data[3]), // weight
+                Integer.parseInt(data[4]), // reps
+                Integer.parseInt(data[5]), // sets
+                date
         );
     }
 }
